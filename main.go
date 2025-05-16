872b6dfb9bfd0023170775c034523e7af6bf008e
anatolio-deb <nikiforova693@gmail.com>
2025-05-16 05:50:02 +0000

    fix

--- main.go
+++ main.go
@@ -124,8 +124,8 @@ func tryHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 				_, err := b.SendMessage(ctx, &bot.SendMessageParams{
 					ChatID: update.Message.Chat.ID,
 					Text: fmt.Sprintf(
-						`<p>Free Trial is activated for your account!</p>
-<p>Use Cisco AnyConnect apps to connect to the VPN:</p>
+						`Free Trial is activated for your account!
+Use Cisco AnyConnect apps to connect to the VPN:
 <ul>
 <li><a href="https://play.google.com/store/apps/details?id=com.cisco.anyconnect.vpn.android.avf&amp;hl=en">Google Play</a></li>
 <li><a href="https://apps.apple.com/ru/app/cisco-secure-client/id1135064690?l=en-GB">AppStore</a></li>
